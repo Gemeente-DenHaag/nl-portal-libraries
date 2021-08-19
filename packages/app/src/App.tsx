94ea7a7a8274cb9be5c921c3d8d32d79efc04418
mritense <maxim.buur@ritense.com>
2021-08-19 12:58:43 +0200

    Work on keycloak and improve root start command

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import './App.css';
 import '@gemeente-denhaag/design-tokens-components';
 import {ExampleComponent} from '@nl-portal/user-interface';
+import {KeycloakComponent} from '@nl-portal/authentication';
 
 function App() {
-  return <ExampleComponent />;
+  return (
+    <KeycloakComponent
+      clientId={`${process.env.REACT_APP_KEYCLOAK_CLIENT_ID}`}
+      realm={`${process.env.REACT_APP_KEYCLOAK_REALM}`}
+      url={`${process.env.REACT_APP_KEYCLOAK_URL}`}
+    >
+      <ExampleComponent />
+    </KeycloakComponent>
+  );
 }
 
 export default App;
--- packages/authentication/src/components/keycloak/keycloak.component.tsx
+++ packages/authentication/src/components/keycloak/keycloak.component.tsx
@@ -1,14 +1,24 @@
 import * as React from 'react';
 import {ReactKeycloakProvider} from '@react-keycloak/web';
 import PropTypes from 'prop-types';
-import keycloak from './keycloak';
+import Keycloak, {KeycloakConfig} from 'keycloak-js';
+import {FC, useState} from 'react';
 
-const KeycloakComponent = ({children}) => (
-  <ReactKeycloakProvider authClient={keycloak}>{children}</ReactKeycloakProvider>
-);
+interface KeycloakProps extends KeycloakConfig {
+  children?: React.ReactNode;
+}
+
+const KeycloakComponent: FC<KeycloakProps> = ({children, url, clientId, realm}) => {
+  const [keycloakClient] = useState(() => new (Keycloak as any)({url, clientId, realm}));
+
+  return <ReactKeycloakProvider authClient={keycloakClient}>{children}</ReactKeycloakProvider>;
+};
 
 KeycloakComponent.propTypes = {
   children: PropTypes.node.isRequired,
+  clientId: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  realm: PropTypes.string.isRequired,
 };
 
-export default KeycloakComponent;
+export {KeycloakComponent};
