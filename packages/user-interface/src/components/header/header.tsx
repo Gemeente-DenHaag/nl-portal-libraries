99b63649fb6b8c8dbb75ff97195e58cea6f62c47
mritense <maxim.buur@ritense.com>
2021-08-25 10:45:35 +0200

    Inner header

--- packages/user-interface/src/components/header/header.tsx
+++ packages/user-interface/src/components/header/header.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import {FC} from 'react';
 import styles from './header.module.scss';
 
-const Header: FC = () => <header className={styles.header} />;
+const Header: FC = () => (
+  <header className={styles.header}>
+    <div className={styles.headerInner} />
+  </header>
+);
 
 export {Header};
