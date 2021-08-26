cb7e8eb90864a8f19823052d15e60681b66ba7b5
mritense <maxim.buur@ritense.com>
2021-08-26 11:14:38 +0200

    Disable custom locales by default

--- packages/app/src/components/app/app.tsx
+++ packages/app/src/components/app/app.tsx
@@ -4,7 +4,7 @@ import '@nl-portal/user-interface/dist/index.css';
 import {Layout} from '@nl-portal/user-interface';
 import {KeycloakWrapper} from '@nl-portal/authentication';
 import {LocalizationProvider} from '@nl-portal/localization';
-import {CUSTOM_MESSAGES, CUSTOM_LOCALES} from '../../i18n';
+import {CUSTOM_MESSAGES} from '../../i18n';
 import {ReactComponent as HeaderLogo} from '../../assets/header-logo.svg';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       url={`${process.env.REACT_APP_KEYCLOAK_URL}`}
       redirectUri={`${process.env.REACT_APP_KEYCLOAK_REDIRECT_URI}`}
     >
-      <LocalizationProvider customMessages={CUSTOM_MESSAGES} customLocales={CUSTOM_LOCALES}>
+      <LocalizationProvider customMessages={CUSTOM_MESSAGES}>
         <Layout headerLogo={<HeaderLogo />} />
       </LocalizationProvider>
     </KeycloakWrapper>
