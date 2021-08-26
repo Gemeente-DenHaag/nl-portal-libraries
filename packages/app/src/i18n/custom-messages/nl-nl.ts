41dab571236e1f615423fbc51b945a56b3a87e8d
mritense <maxim.buur@ritense.com>
2021-08-26 13:56:03 +0200

    Remove message overide

--- packages/app/src/i18n/custom-messages/nl-nl.ts
+++ packages/app/src/i18n/custom-messages/nl-nl.ts
@@ -2,7 +2,6 @@ import {DEFAULT_LOCALES, Messages} from '@nl-portal/localization';
 
 export const NL_NL_MESSAGES: Messages = {
   [DEFAULT_LOCALES.DUTCH]: {
-    'app.logout': 'Afmelden',
     'app.testMessage': 'Testbericht',
   },
 };
