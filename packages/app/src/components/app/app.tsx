371bb0eda1c1d51d657261ecbd552f57814089f8
mritense <maxim.buur@ritense.com>
2021-08-27 11:56:18 +0200

    Use nl portal design tokens for header styling

--- packages/app/src/components/app/app.tsx
+++ packages/app/src/components/app/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@gemeente-denhaag/design-tokens-components';
 import '@nl-portal/user-interface/dist/index.css';
+import '../../styles/nl-portal-design-tokens.css';
 import {Layout} from '@nl-portal/user-interface';
 import {KeycloakWrapper} from '@nl-portal/authentication';
 import {LocalizationProvider} from '@nl-portal/localization';
