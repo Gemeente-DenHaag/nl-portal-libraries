8e60ce160c54c8e97dad887d89d01863cd4ea632
mritense <maxim.buur@ritense.com>
2021-08-27 12:59:31 +0200

    Use BEM class names for header

--- packages/user-interface/src/components/header/header.tsx
+++ packages/user-interface/src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {FC} from 'react';
+import {FC, ReactElement} from 'react';
 import {ShowAt} from 'react-hide-show-utils';
 import styles from './header.module.scss';
 import {LanguageSwitcher} from '../language-switcher';
@@ -9,19 +9,25 @@ import {MenuButton} from '../menu-button';
 import {constants} from '../../constants';
 
 interface HeaderProps {
-  logo: React.ReactNode;
-  facet?: React.ReactNode;
+  logo: ReactElement;
+  facet?: ReactElement;
 }
 
 const Header: FC<HeaderProps> = ({logo, facet}) => (
-  <div className={styles.headerContainer}>
+  <div className={styles['header-wrapper']}>
     <header className={styles.header}>
-      <div className={styles.headerInner}>
-        <div className={styles.headerLogoContainer}>{logo}</div>
-        <div className={styles.headerElementsContainer}>
+      <div className={styles.header__inner}>
+        <div className={styles['header__logo-container']}>
+          {React.cloneElement(logo, {className: styles['header__logo-image']})}
+        </div>
+        <div className={styles['header__elements-container']}>
           <ShowAt breakpoint={constants.BREAKPOINT_HEADER_MIN}>
-            <UserName />
-            <Logout />
+            <div className={styles['header__element--large-spacing']}>
+              <UserName />
+            </div>
+            <div className={styles['header__element--medium-spacing']}>
+              <Logout />
+            </div>
             <LanguageSwitcher />
           </ShowAt>
           <ShowAt breakpoint={constants.BREAKPOINT_HEADER_MAX}>
@@ -30,7 +36,11 @@ const Header: FC<HeaderProps> = ({logo, facet}) => (
         </div>
       </div>
     </header>
-    {facet && <div className={styles.headerFacetContainer}>{facet}</div>}
+    {facet && (
+      <div className={styles['header__facet-container']}>
+        {React.cloneElement(facet, {className: styles['header__facet-image']})}
+      </div>
+    )}
   </div>
 );
 
