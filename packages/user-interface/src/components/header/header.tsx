d108384b5ea9e8ee7b8ab7d900bea7b950b3fb4f
mritense <maxim.buur@ritense.com>
2021-08-30 14:51:30 +0200

    Improve breakpoints

--- packages/user-interface/src/components/header/header.tsx
+++ packages/user-interface/src/components/header/header.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import {FC, ReactElement} from 'react';
-import {ShowAt} from 'react-hide-show-utils';
 import styles from './header.module.scss';
 import {LanguageSwitcher} from '../language-switcher';
 import {Logout} from '../logout';
 import {UserName} from '../user-name';
 import {MenuButton} from '../menu-button';
-import {constants} from '../../constants';
 
 interface HeaderProps {
   logo: ReactElement;
@@ -20,19 +18,17 @@ const Header: FC<HeaderProps> = ({logo, facet}) => (
         <div className={styles['header__logo-container']}>
           {React.cloneElement(logo, {className: styles['header__logo-image']})}
         </div>
-        <div className={styles['header__elements-container']}>
-          <ShowAt breakpoint={constants.BREAKPOINT_HEADER_MIN}>
-            <div className={styles['header__element--large-spacing']}>
-              <UserName />
-            </div>
-            <div className={styles['header__element--medium-spacing']}>
-              <Logout />
-            </div>
-            <LanguageSwitcher />
-          </ShowAt>
-          <ShowAt breakpoint={constants.BREAKPOINT_HEADER_MAX}>
-            <MenuButton />
-          </ShowAt>
+        <div className={styles['header__elements-mobile']}>
+          <MenuButton />
+        </div>
+        <div className={styles['header__elements-desktop']}>
+          <div className={styles['header__element--large-spacing']}>
+            <UserName />
+          </div>
+          <div className={styles['header__element--medium-spacing']}>
+            <Logout />
+          </div>
+          <LanguageSwitcher />
         </div>
       </div>
     </header>
--- packages/user-interface/src/constants/constants.ts
+++
@@ -1,4 +0,0 @@
-export const constants = {
-  BREAKPOINT_HEADER_MIN: '(min-width: 768px)',
-  BREAKPOINT_HEADER_MAX: '(max-width: 767px)',
-};
