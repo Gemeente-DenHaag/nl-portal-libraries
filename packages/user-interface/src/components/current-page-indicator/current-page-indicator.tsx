84f4237ca67e49e81b7c3db72e6362b2c8b2e9f6
mritense <maxim.buur@ritense.com>
2021-09-03 15:01:20 +0200

    Finish current page indicator

--- packages/user-interface/src/components/current-page-indicator/current-page-indicator.tsx
+++ packages/user-interface/src/components/current-page-indicator/current-page-indicator.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {useContext} from 'react';
 import {FormattedMessage} from 'react-intl';
 import {Heading5} from '@gemeente-denhaag/denhaag-component-library';
+import {ChevronDownIcon} from '@gemeente-denhaag/icons';
 import {LayoutContext} from '../../contexts';
 import styles from './current-page-indicator.module.scss';
 
@@ -9,13 +10,16 @@ const CurrentPageIndicator = () => {
   const {currentPage} = useContext(LayoutContext);
   return (
     <div className={styles['current-page-indicator']}>
-      <div className={styles['current-page-indicator__inner']}>
+      <header className={styles['current-page-indicator__header']}>
         {currentPage?.icon && (
           <div className={styles['current-page-indicator__icon']}>{currentPage.icon}</div>
         )}
         <Heading5>
           <FormattedMessage id={`pageTitles.${currentPage?.titleTranslationKey}`} />
         </Heading5>
+      </header>
+      <div className={styles['current-page-indicator__chevron-down']}>
+        <ChevronDownIcon />
       </div>
     </div>
   );
