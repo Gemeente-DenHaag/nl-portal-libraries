15fc6cff6b6623eff509c53916a47d3b6e35183b
mritense <maxim.buur@ritense.com>
2021-09-06 14:11:07 +0200

    Show mock cases

---
+++ packages/user-interface/src/pages/cases/cases-page-mock.ts
@@ -0,0 +1,19 @@
+import {PortalCase} from '../../interfaces';
+
+export const mockCases: Array<PortalCase> = [
+  {
+    createdOn: new Date(2021, 1, 1),
+    id: 'xxx',
+    type: 'parking-permit',
+  },
+  {
+    createdOn: new Date(2021, 1, 3),
+    id: 'xxx',
+    type: 'holiday-rental',
+  },
+  {
+    createdOn: new Date(2021, 1, 5),
+    id: 'xxx',
+    type: 'parking-permit',
+  },
+];
--- packages/user-interface/src/pages/cases/cases-page.tsx
+++ packages/user-interface/src/pages/cases/cases-page.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Heading2, Card} from '@gemeente-denhaag/denhaag-component-library';
 import {FormattedMessage} from 'react-intl';
 import styles from './cases-page.module.scss';
+import {mockCases} from './cases-page-mock';
 
 const CasesPage = () => (
   <section className={styles.cases}>
@@ -10,7 +11,13 @@ const CasesPage = () => (
         <FormattedMessage id="pageTitles.cases" />
       </Heading2>
     </header>
-    <Card variant="case" title="test" date={new Date()} href="./" />
+    <div className={styles.cases__cards}>
+      {mockCases.map(mockCase => (
+        <div className={styles.cases__card}>
+          <Card variant="case" title={mockCase.type} date={mockCase.createdOn} href="./" />
+        </div>
+      ))}
+    </div>
   </section>
 );
 export {CasesPage};
