53d44dcd6bf475566bb5ee0d47bdd3e75c064838
mritense <maxim.buur@ritense.com>
2021-09-06 17:56:08 +0200

    Add completed case mocks

--- packages/user-interface/src/pages/cases/cases-page-mock.ts
+++ packages/user-interface/src/pages/cases/cases-page-mock.ts
@@ -16,4 +16,40 @@ export const mockCases: Array<PortalCase> = [
     id: '3',
     type: 'parking-permit',
   },
+  {
+    createdOn: new Date(2021, 1, 1),
+    id: '1',
+    type: 'parking-permit',
+    completed: true,
+  },
+  {
+    createdOn: new Date(2021, 1, 3),
+    id: '2',
+    type: 'holiday-rental',
+    completed: true,
+  },
+  {
+    createdOn: new Date(2021, 1, 5),
+    id: '3',
+    type: 'parking-permit',
+    completed: true,
+  },
+  {
+    createdOn: new Date(2021, 1, 6),
+    id: '4',
+    type: 'parking-permit',
+    completed: true,
+  },
+  {
+    createdOn: new Date(2021, 1, 9),
+    id: '5',
+    type: 'holiday-rental',
+    completed: true,
+  },
+  {
+    createdOn: new Date(2021, 1, 11),
+    id: '6',
+    type: 'parking-permit',
+    completed: true,
+  },
 ];
