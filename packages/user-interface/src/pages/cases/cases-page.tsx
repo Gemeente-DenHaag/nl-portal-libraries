f3342d7845843deab7ae0b13ef9dc1b3df91e357
mritense <maxim.buur@ritense.com>
2021-09-06 12:21:57 +0200

    Work on cases page

--- packages/user-interface/src/pages/cases/cases-page.tsx
+++ packages/user-interface/src/pages/cases/cases-page.tsx
@@ -1,4 +1,16 @@
 import * as React from 'react';
+import {Heading2, Card} from '@gemeente-denhaag/denhaag-component-library';
+import {FormattedMessage} from 'react-intl';
+import styles from './cases-page.module.scss';
 
-const CasesPage = () => <div>cases</div>;
+const CasesPage = () => (
+  <section className={styles.cases}>
+    <header className={styles.cases__header}>
+      <Heading2>
+        <FormattedMessage id="pageTitles.cases" />
+      </Heading2>
+    </header>
+    <Card variant="case" title="test" date={new Date()} href="./" />
+  </section>
+);
 export {CasesPage};
