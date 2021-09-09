d6ab1279f6cda7c8fcff243bb8a8de12a459b4fd
Test-Automation-Engineer <mslingerland@gmail.com>
2021-09-10 00:01:05 +0200

    Change to not inplace

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
-ls -al /usr/share/nginx/html
 if [ ! -z "$KEYCLOAK_URL" ]; then
     echo "Set KEYCLOAK_URL to $KEYCLOAK_URL"
-    sed -i "s/\(KEYCLOAK_URL = '\).*\('\)/\1$KEYCLOAK_URL\2/g" /usr/share/nginx/html/config.js
-fi
+    sed "s/\(KEYCLOAK_URL = '\).*\('\)/\1$KEYCLOAK_URL\2/g" /usr/share/nginx/html/config.js > /tmp/my.js && cat /tmp/my.js > /usr/share/nginx/html/config.js
 if [ ! -z "$KEYCLOAK_REALM" ]; then
     echo "Set KEYCLOAK_REALM to $KEYCLOAK_REALM"
-    sed -i "s/\(KEYCLOAK_REALM = '\).*\('\)/\1$KEYCLOAK_REALM\2/g" /usr/share/nginx/html/config.js
+    sed "s/\(KEYCLOAK_REALM = '\).*\('\)/\1$KEYCLOAK_REALM\2/g" /usr/share/nginx/html/config.js > /tmp/my.js && cat /tmp/my.js > /usr/share/nginx/html/config.js
 fi
 if [ ! -z "$KEYCLOAK_CLIENT_ID" ]; then
     echo "Set KEYCLOAK_CLIENT_ID to $KEYCLOAK_CLIENT_ID"
-    sed -i "s/\(KEYCLOAK_CLIENT_ID = '\).*\('\)/\1$KEYCLOAK_CLIENT_ID\2/g" /usr/share/nginx/html/config.js
+    sed "s/\(KEYCLOAK_CLIENT_ID = '\).*\('\)/\1$KEYCLOAK_CLIENT_ID\2/g" /usr/share/nginx/html/config.js > /tmp/my.js && cat /tmp/my.js > /usr/share/nginx/html/config.js
 fi
 if [ ! -z "$KEYCLOAK_REDIRECT_URI" ]; then
     echo "Set KEYCLOAK_REDIRECT_URI to $KEYCLOAK_REDIRECT_URI"
-    sed -i "s/\(KEYCLOAK_REDIRECT_URI = '\).*\('\)/\1$KEYCLOAK_REDIRECT_URI\2/g" /usr/share/nginx/html/config.js
+    sed "s/\(KEYCLOAK_REDIRECT_URI = '\).*\('\)/\1$KEYCLOAK_REDIRECT_URI\2/g" /usr/share/nginx/html/config.js > /tmp/my.js && cat /tmp/my.js > /usr/share/nginx/html/config.js
 fi
