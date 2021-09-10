9d5725a4f626ec308bd0aec5fcdcdb2b99260de4
Test-Automation-Engineer <mslingerland@gmail.com>
2021-09-10 09:07:50 +0200

    touch tmp my.js

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+touch /tmp/my.js
+ls -al /tmp
+
 if [ ! -z "$KEYCLOAK_URL" ]; then
     echo "Set KEYCLOAK_URL to $KEYCLOAK_URL"
     sed "s/\(KEYCLOAK_URL = '\).*\('\)/\1$KEYCLOAK_URL\2/g" /usr/share/nginx/html/config.js > /tmp/my.js && cat /tmp/my.js > /usr/share/nginx/html/config.js
+fi
 if [ ! -z "$KEYCLOAK_REALM" ]; then
     echo "Set KEYCLOAK_REALM to $KEYCLOAK_REALM"
     sed "s/\(KEYCLOAK_REALM = '\).*\('\)/\1$KEYCLOAK_REALM\2/g" /usr/share/nginx/html/config.js > /tmp/my.js && cat /tmp/my.js > /usr/share/nginx/html/config.js
