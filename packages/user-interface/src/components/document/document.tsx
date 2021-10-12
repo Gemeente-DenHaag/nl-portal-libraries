7bf9f28fe2a88a6ecdc3b7a9269de6986917aa03
mritense <maxim.buur@ritense.com>
2021-10-12 11:15:16 +0200

    Work on document component

--- packages/user-interface/src/components/document/document.tsx
+++ packages/user-interface/src/components/document/document.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {FC} from 'react';
+import {DocumentIcon, ExternalLinkIcon} from '@gemeente-denhaag/icons';
+import {Link, Paragraph} from '@gemeente-denhaag/denhaag-component-library';
 import {PortalDocument} from '../../interfaces';
 import styles from './document.module.scss';
 
@@ -7,10 +9,15 @@ type DocumentProps = Partial<PortalDocument>;
 
 const Document: FC<DocumentProps> = ({url, extension, name, size}) => (
   <div className={styles.document}>
-    {url}
-    {extension}
-    {name}
-    {size}
+    <div className={styles['document__icon-container']}>
+      <DocumentIcon className={styles.document__icon} />
+    </div>
+    <div className={styles.document__content}>
+      <Paragraph>{`${name} (${extension}, ${size}kb)`}</Paragraph>
+      <Link iconAlign="start" icon={<ExternalLinkIcon />} href={url}>
+        Download
+      </Link>
+    </div>
   </div>
 );
 
