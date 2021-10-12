b78f773919a9e943a72f6446e60544e6f3a5f906
mritense <maxim.buur@ritense.com>
2021-10-12 11:48:37 +0200

    Work on document component

--- packages/user-interface/src/components/document/document.tsx
+++ packages/user-interface/src/components/document/document.tsx
@@ -2,23 +2,36 @@ import * as React from 'react';
 import {FC} from 'react';
 import {DocumentIcon, ExternalLinkIcon} from '@gemeente-denhaag/icons';
 import {Link, Paragraph} from '@gemeente-denhaag/denhaag-component-library';
+import classNames from 'classnames';
 import {PortalDocument} from '../../interfaces';
 import styles from './document.module.scss';
+import {useMediaQuery} from '../../hooks';
+import {BREAKPOINTS} from '../../constants';
 
 type DocumentProps = Partial<PortalDocument>;
 
-const Document: FC<DocumentProps> = ({url, extension, name, size}) => (
-  <div className={styles.document}>
-    <div className={styles['document__icon-container']}>
-      <DocumentIcon className={styles.document__icon} />
+const Document: FC<DocumentProps> = ({url, extension, name, size}) => {
+  const isDesktop = useMediaQuery(BREAKPOINTS.DESKTOP);
+
+  return (
+    <div className={styles.document}>
+      <div className={styles['document__icon-container']}>
+        <DocumentIcon className={styles.document__icon} />
+      </div>
+      <div
+        className={classNames(styles.document__content, {
+          [styles['document__content--desktop']]: isDesktop,
+        })}
+      >
+        <Paragraph
+          className={styles['document__file-name']}
+        >{`${name} (${extension}, ${size}kb)`}</Paragraph>
+        <Link iconAlign="start" icon={<ExternalLinkIcon />} href={url}>
+          Download
+        </Link>
+      </div>
     </div>
-    <div className={styles.document__content}>
-      <Paragraph>{`${name} (${extension}, ${size}kb)`}</Paragraph>
-      <Link iconAlign="start" icon={<ExternalLinkIcon />} href={url}>
-        Download
-      </Link>
-    </div>
-  </div>
-);
+  );
+};
 
 export {Document};
