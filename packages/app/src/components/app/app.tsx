fd65dd9b6092f3f13cbacf47944ad398f6817f63
mritense <maxim.buur@ritense.com>
2021-10-14 13:28:53 +0200

    Remove separate footer and pages config files due to circular dependency errors

--- packages/app/src/components/app/app.tsx
+++ packages/app/src/components/app/app.tsx
@@ -6,12 +6,121 @@ import {KeycloakWrapper} from '@nl-portal/authentication';
 import {LocalizationProvider} from '@nl-portal/localization';
 import {ApolloWrapper} from '@nl-portal/api';
 import {Offline, Online} from 'react-detect-offline';
-import {Layout} from '@nl-portal/user-interface';
+import {
+  CasePage,
+  CasesPage,
+  DocumentsPage,
+  Layout,
+  NotificationsPage,
+  OverviewPage,
+  PortalFooter,
+  PortalPage,
+  ThemesPage,
+} from '@nl-portal/user-interface';
+import {ArchiveIcon, DocumentIcon, GridIcon, InboxIcon} from '@gemeente-denhaag/icons';
 import {CUSTOM_MESSAGES} from '../../i18n';
 import {ReactComponent as HeaderLogo} from '../../assets/header-logo.svg';
 import Facet from '../../assets/facet.png';
 import {config} from '../../config';
-import {footer, pages} from '../../configuration';
+import StatusHistoryBackground from '../../assets/status-history-background.svg';
+
+const pages: Array<PortalPage> = [
+  {
+    icon: <GridIcon />,
+    pageComponent: <OverviewPage />,
+    path: '/',
+    titleTranslationKey: 'overview',
+    showInMenu: true,
+    isHome: true,
+  },
+  {
+    icon: <InboxIcon />,
+    pageComponent: <NotificationsPage />,
+    path: '/berichten',
+    titleTranslationKey: 'notifications',
+    showInMenu: true,
+    showMessagesCount: true,
+  },
+  {
+    icon: <ArchiveIcon />,
+    pageComponent: <CasesPage />,
+    path: '/zaken',
+    titleTranslationKey: 'cases',
+    showInMenu: true,
+    children: [
+      {
+        icon: <ArchiveIcon />,
+        pageComponent: (
+          <CasePage
+            statusHistoryFacet={<img src={Facet} alt="" />}
+            statusHistoryBackground={<img src={StatusHistoryBackground} alt="" />}
+          />
+        ),
+        path: '/zaak',
+        titleTranslationKey: 'cases',
+        showLinkToParent: true,
+      },
+      {
+        icon: <ArchiveIcon />,
+        pageComponent: <DocumentsPage />,
+        path: '/zaak/documenten',
+        titleTranslationKey: 'cases',
+      },
+    ],
+  },
+  {
+    icon: <DocumentIcon />,
+    pageComponent: <ThemesPage />,
+    path: '/themas',
+    titleTranslationKey: 'themes',
+    showInMenu: true,
+  },
+];
+
+const footer: PortalFooter = [
+  {
+    titleTranslationKey: 'theHague',
+    links: [
+      {linkTranslationKey: 'goToTheHague', url: 'https://www.denhaag.nl/nl.htm', hrefLang: 'nl'},
+      {linkTranslationKey: 'goToTheHague', url: 'https://www.denhaag.nl/en.htm', hrefLang: 'en'},
+    ],
+  },
+  {
+    titleTranslationKey: 'disclaimers',
+    links: [
+      {
+        linkTranslationKey: 'accessibility',
+        url: 'https://www.denhaag.nl/nl/toegankelijkheidsverklaring.htm',
+        hrefLang: 'nl',
+      },
+      {
+        linkTranslationKey: 'accessibility',
+        url: 'https://www.denhaag.nl/en/toegankelijkheidsverklaring.htm',
+        hrefLang: 'en',
+      },
+      {
+        linkTranslationKey: 'dataProtection',
+        url: 'https://www.denhaag.nl/nl/verklaring-inzake-gegevensbescherming.htm',
+        hrefLang: 'nl',
+      },
+      {
+        linkTranslationKey: 'dataProtection',
+        url: 'https://www.denhaag.nl/en/data-protection-declaration.htm',
+        hrefLang: 'en',
+      },
+      {
+        linkTranslationKey: 'proclaimer',
+        url: 'https://www.denhaag.nl/home/algemeen/proclaimer.htm',
+        hrefLang: 'nl',
+      },
+      {
+        linkTranslationKey: 'proclaimer',
+        url: 'https://www.denhaag.nl/en/proclaimer.htm',
+        hrefLang: 'en',
+      },
+    ],
+  },
+];
 
 const App = () => (
   <Fragment>
