8ea8332f170d82e88f96e5a2e280e69392b3b63d
mritense <79840403+mritense@users.noreply.github.com>
2021-10-27 14:30:04 +0200

    Change open forms location (#34)

--- packages/user-interface/src/pages/form/form-page.tsx
+++ packages/user-interface/src/pages/form/form-page.tsx
@@ -8,17 +8,17 @@ declare const OpenForms: any;
 
 const FormPage = () => {
   const openFormsScript = useScript(
-    'https://denhaag.test.open-formulieren.nl/sdk/open-forms-sdk.js'
+    'https://openformulieren-cg.test.denhaag.nl/static/sdk/open-forms-sdk.js'
   );
 
   useEffect(() => {
     if (typeof OpenForms !== 'undefined') {
       const formId = 'bezwaar\u002Dmaken';
-      const baseUrl = 'https://denhaag.test.open\u002Dformulieren.nl/api/v1/';
+      const baseUrl = 'https://openformulieren\u002Dcg.test.denhaag.nl/api/v1/';
       const targetNode = document.getElementById('openforms\u002Dcontainer');
       const basePath = '/bezwaar\u002Dmaken/';
 
-      const sentryEnv = 'denhaag\u002Dtest';
+      const sentryEnv = 'docker';
       const form = new OpenForms.OpenForm(targetNode, {baseUrl, formId, basePath, sentryEnv});
       form.init();
     }
@@ -28,7 +28,7 @@ const FormPage = () => {
     <div>
       <Helmet>
         <link
-          href="https://denhaag.test.open-formulieren.nl/sdk/open-forms-sdk.css"
+          href="https://openformulieren-cg.test.denhaag.nl/static/sdk/open-forms-sdk.css"
           rel="stylesheet"
         />
       </Helmet>
