1821d8d56c4d6b0f84afd5c06caa7bf54935cf2b
mritense <maxim.buur@ritense.com>
2021-11-02 10:49:26 +0100

    Working theme switcher component

--- packages/user-interface/src/components/theme-switcher/theme-switcher.tsx
+++ packages/user-interface/src/components/theme-switcher/theme-switcher.tsx
@@ -1,26 +1,33 @@
 import * as React from 'react';
-import {useEffect} from 'react';
+import {Fragment, useEffect, useState} from 'react';
 import {Helmet} from 'react-helmet';
 import {useScript} from 'usehooks-ts';
 
 const ThemeSwitcher = () => {
+  const [scriptLoaded, setScriptLoaded] = useState(false);
   const themeSwitcherScript = useScript(
     'https://unpkg.com/@nl-design-system-unstable/theme-switcher'
   );
 
   useEffect(() => {
-    console.log(themeSwitcherScript);
+    if (themeSwitcherScript === 'ready') {
+      setScriptLoaded(true);
+    }
   }, [themeSwitcherScript]);
 
   return (
-    <div>
+    <Fragment>
       <Helmet>
         <link
           href="https://unpkg.com/@utrecht/component-library-css/dist/index.css"
           rel="stylesheet"
         />
       </Helmet>
-    </div>
+      {scriptLoaded && (
+        // @ts-ignore
+        <nl-theme-switcher />
+      )}
+    </Fragment>
   );
 };
 
