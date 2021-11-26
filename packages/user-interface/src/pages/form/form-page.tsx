0ca86f0998c219289b3b68b16cb2e22ad75a8635
Bryan de Jong <bryandejong99@gmail.com>
2021-11-26 14:30:31 +0100

    feat: add nlds-open-forms-sdk

--- packages/user-interface/src/pages/form/form-page.tsx
+++ packages/user-interface/src/pages/form/form-page.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import {useContext, useEffect} from 'react';
-import {Helmet} from 'react-helmet';
-import {useScript} from 'usehooks-ts';
 import {LayoutContext} from '../../contexts';
 
 // eslint-disable-next-line
@@ -10,9 +8,6 @@ declare const OpenForms: any;
 const FormPage = () => {
   const {enableFullscreenForm, disableFullscreenForm, setCurrentFormTitle, clearCurrentFormTitle} =
     useContext(LayoutContext);
-  const openFormsScript = useScript(
-    'https://openformulieren-cg.test.denhaag.nl/static/sdk/open-forms-sdk.js'
-  );
 
   useEffect(() => {
     enableFullscreenForm();
@@ -47,16 +42,10 @@ const FormPage = () => {
 
       checkFormName();
     }
-  }, [openFormsScript]);
+  }, []);
 
   return (
     <div>
-      <Helmet>
-        <link
-          href="https://openformulieren-cg.test.denhaag.nl/static/sdk/open-forms-sdk.css"
-          rel="stylesheet"
-        />
-      </Helmet>
       <div id="openforms-container" />
     </div>
   );
