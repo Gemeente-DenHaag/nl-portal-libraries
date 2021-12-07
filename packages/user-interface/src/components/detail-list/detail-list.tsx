5b5bee54e46544d93ae0f0852329e18d4d91b003
mritense <maxim.buur@ritense.com>
2021-12-07 09:39:22 +0100

    Fix button, add loading anim

--- packages/user-interface/src/components/detail-list/detail-list.tsx
+++ packages/user-interface/src/components/detail-list/detail-list.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import {FC, Fragment} from 'react';
+import {FC, Fragment, useEffect, useState} from 'react';
 import {FormattedMessage} from 'react-intl';
 import {IconButton, Button} from '@gemeente-denhaag/components-react';
 import {EditIcon} from '@gemeente-denhaag/icons';
+import Skeleton from 'react-loading-skeleton';
 import styles from './detail-list.module.scss';
 import {useMediaQuery} from '../../hooks';
 import {BREAKPOINTS} from '../../constants';
@@ -12,7 +13,14 @@ interface DetailListProps {
 }
 
 const DetailList: FC<DetailListProps> = ({details}) => {
+  const [loading, setLoading] = useState(true);
   const isTablet = useMediaQuery(BREAKPOINTS.TABLET);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
 
   return (
     <Fragment>
@@ -24,7 +32,9 @@ const DetailList: FC<DetailListProps> = ({details}) => {
             </b>
           </span>
           <div className={styles['detail-list__value-edit']}>
-            <span className={styles['detail-list__value']}>{detail.value}</span>
+            <span className={styles['detail-list__value']}>
+              {loading ? <Skeleton width={isTablet ? 250 : 150} /> : detail.value}
+            </span>
             {isTablet ? (
               <Button icon={<EditIcon />} iconAlign="start">
                 <FormattedMessage id="account.edit" />
