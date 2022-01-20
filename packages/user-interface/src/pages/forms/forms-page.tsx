dff9a1a5fc67faf083655d7ed73dc07443b74b73
mritense <maxim.buur@ritense.com>
2022-01-20 15:56:37 +0100

    basic setup forms page

--- packages/user-interface/src/pages/forms/forms-page.tsx
+++ packages/user-interface/src/pages/forms/forms-page.tsx
@@ -1,4 +1,15 @@
 import * as React from 'react';
+import {Heading2} from '@gemeente-denhaag/components-react';
+import {FormattedMessage} from 'react-intl';
+import styles from './forms-page.module.scss';
 
-const FormsPage = () => <div>forms</div>;
+const FormsPage = () => (
+  <section className={styles.forms}>
+    <header className={styles.forms__header}>
+      <Heading2>
+        <FormattedMessage id="pageTitles.forms" />
+      </Heading2>
+    </header>
+  </section>
+);
 export {FormsPage};
