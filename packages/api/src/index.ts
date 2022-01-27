f9f03ef08ac242cb0974eff61425c902ad430cd8
mritense <maxim.buur@ritense.com>
2022-01-27 10:52:19 +0100

    add format trailing slash util with test

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,3 +1,4 @@
 export * from './generated';
 export * from './components';
 export * from './contexts';
+export * from './utils';
---
+++ packages/api/src/utils/format-url-trailing-slash.ts
@@ -0,0 +1,19 @@
+export const formatUrlTrailingSlash = (url: string, returnWithTrailingSlash: boolean): string => {
+  const urlLastCharacter = url[url.length - 1];
+  const urlLastCharacterIsSlash = urlLastCharacter === '/';
+
+  if (
+    (returnWithTrailingSlash && urlLastCharacterIsSlash) ||
+    (!returnWithTrailingSlash && !urlLastCharacterIsSlash)
+  ) {
+    return url;
+  }
+  if (!returnWithTrailingSlash && urlLastCharacterIsSlash) {
+    return url.slice(0, -1);
+  }
+  if (returnWithTrailingSlash && !urlLastCharacterIsSlash) {
+    return `${url}/`;
+  }
+
+  return url;
+};
