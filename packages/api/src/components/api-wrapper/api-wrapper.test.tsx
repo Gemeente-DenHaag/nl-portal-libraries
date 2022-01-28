7531fe4179852118d8743f44da24bc5a936ca382
Test-Automation-Engineer <mslingerland@gmail.com>
2022-01-28 09:10:03 +0100

    Undo fail test

--- packages/api/src/components/api-wrapper/api-wrapper.test.tsx
+++ packages/api/src/components/api-wrapper/api-wrapper.test.tsx
@@ -2,6 +2,6 @@ import {ApiWrapper} from './api-wrapper';
 
 describe('ApiWrapper', () => {
   it('is truthy', () => {
-    expect(ApiWrapper).toBeFalsy();
+    expect(ApiWrapper).toBeTruthy();
   });
 });
