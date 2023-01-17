f82d3435076b5e730aeb77fb3af89bb75eb9c792
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-01-17 15:14:16 +0100

    feat: added some extra configuration options to show/hide content

--- packages/user-interface/src/pages/overview/overview-page.tsx
+++ packages/user-interface/src/pages/overview/overview-page.tsx
@@ -1,22 +1,40 @@
 import * as React from 'react';
 import {Link as RouterLink} from 'react-router-dom';
-import {Link} from '@gemeente-denhaag/components-react';
+import {Link, Paragraph} from '@gemeente-denhaag/components-react';
 import {FormattedMessage} from 'react-intl';
 import {FC, useContext} from 'react';
 import {LocaleContext} from '@gemeente-denhaag/nl-portal-localization';
 
 interface OverviewPageProps {
-  openFormsFormId: string;
+  openFormsFormId?: string;
+  showFormsLink?: string;
+  showIntro?: string;
 }
 
-const OverviewPage: FC<OverviewPageProps> = ({openFormsFormId}) => {
+const OverviewPage: FC<OverviewPageProps> = ({
+  openFormsFormId,
+  showFormsLink = 'true',
+  showIntro = 'false',
+}) => {
   const {hrefLang} = useContext(LocaleContext);
 
   return (
     <section>
-      <Link component={RouterLink} to={`/formulier/${openFormsFormId}`} hrefLang={hrefLang}>
-        <FormattedMessage id="overview.defaultFormTitle" />
-      </Link>
+      {showIntro === 'true' && (
+        <React.Fragment>
+          <h2 className="utrecht-heading-2">
+            <FormattedMessage id="overviewpage.title" />
+          </h2>
+          <Paragraph>
+            <FormattedMessage id="overviewpage.paragraph" />
+          </Paragraph>
+        </React.Fragment>
+      )}
+      {showFormsLink === 'true' && (
+        <Link component={RouterLink} to={`/formulier/${openFormsFormId}`} hrefLang={hrefLang}>
+          <FormattedMessage id="overview.defaultFormTitle" />
+        </Link>
+      )}
     </section>
   );
 };
