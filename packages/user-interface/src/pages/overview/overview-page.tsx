a58ce89219813a4b687a8e23db8ab78c58f94193
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-04-18 09:54:11 +0200

    feat: added configurable alert option for maintenance

--- packages/user-interface/src/pages/overview/overview-page.tsx
+++ packages/user-interface/src/pages/overview/overview-page.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {Link as RouterLink} from 'react-router-dom';
+import {useIntl} from 'react-intl';
 import {Link, Paragraph} from '@gemeente-denhaag/components-react';
+import {Alert} from '@gemeente-denhaag/alert';
 import {FormattedMessage} from 'react-intl';
 import {FC, useContext} from 'react';
 import {LocaleContext} from '@gemeente-denhaag/nl-portal-localization';
@@ -9,17 +11,28 @@ interface OverviewPageProps {
   openFormsFormId?: string;
   showFormsLink?: string;
   showIntro?: string;
+  showAlert?: string;
+  alertType?: 'error' | 'info' | 'success' | 'warning';
 }
 
 const OverviewPage: FC<OverviewPageProps> = ({
   openFormsFormId,
   showFormsLink = 'true',
   showIntro = 'false',
+  showAlert = 'false',
+  alertType = 'warning',
 }) => {
   const {hrefLang} = useContext(LocaleContext);
+  const intl = useIntl();
+
+  const alertTitle = intl.formatMessage({id: 'overview.alertTitle'});
+  const alertText = intl.formatMessage({id: 'overview.alertText'});
 
   return (
     <section>
+      {showAlert === 'true' && (
+        <Alert variant={alertType} title={alertTitle} text={alertText} />
+      )}
       {showIntro === 'true' && (
         <React.Fragment>
           <h2 className="utrecht-heading-2">
